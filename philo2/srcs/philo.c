532466bf163c48d9b65e333218da8c35cd0220e3
LeonMaurice <72357911+LeonMauriceArt@users.noreply.github.com>
2022-08-13 19:24:29 +0200

    philo done 1st push

--- philo2/srcs/philo.c
+++ philo2/srcs/philo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: leonard <leonard@student.42.fr>            +#+  +:+       +#+        */
+/*   By: lmaurin- <lmaurin-@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/06 00:11:05 by lmaurin           #+#    #+#             */
-/*   Updated: 2022/08/13 17:11:42 by leonard          ###   ########.fr       */
+/*   Updated: 2022/08/13 19:23:02 by lmaurin-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	philo_log(t_philo *philo, char *msg)
 {
 	pthread_mutex_lock(&philo->rules->msg_display);
 	if (!mutex_check_program(philo->rules, 'r'))
-		printf("%lu %d %s\n", get_time() - philo->rules->start_time, \
+		printf("%llu %d %s\n", get_time() - philo->rules->start_time, \
 					philo->id, msg);
 	pthread_mutex_unlock(&philo->rules->msg_display);
 }
@@ -29,8 +29,11 @@ void	take_forks(t_philo *p)
 		philo_log(p, "has taken a fork");
 		pthread_mutex_lock(p->rfork);
 		philo_log(p, "has taken a fork");
-		philo_log(p, "is eating");
-		my_usleep(p->rules->time_to_eat, p->rules);
+		if (!mutex_check_program(p->rules, 'r'))
+		{
+			philo_log(p, "is eating");
+			my_usleep(p->rules->time_to_eat, p->rules);
+		}
 		mutex_check_time(p, 'w');
 		pthread_mutex_unlock(p->lfork);
 		pthread_mutex_unlock(p->rfork);
